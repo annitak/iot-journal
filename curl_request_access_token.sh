865119c667d6be029c05ef55351b8a3971e63bf3
Annita <annita.kuo@gmail.com>
2017-04-07 15:54:59 -0700

    Update now records and processes audio journal

--- curl_request_access_token.sh
+++ curl_request_access_token.sh
@@ -1,4 +1,18 @@
+#!/bin/bash
+
+# Must be in the same directory as service_account_file.json
+export GOOGLE_APPLICATION_CREDENTIALS=service_account_file.json
+gcloud auth activate-service-account --key-file=service_account_file.json
+ACCESSTOKEN=`gcloud auth print-access-token`
+
+
 curl -s -k -H "Content-Type: application/json" \
-     -H "Authorization: Bearer access_token" \
+     -H "Authorization: Bearer $ACCESSTOKEN" \
      https://speech.googleapis.com/v1beta1/speech:syncrecognize \
-         -d @sync-request.json
+         -d @sync-request.json \
+
+# Records, processes, and transcribes journal entry to journal.txt
+python recorder.py
+echo "Recording completed. Transcribing..."
+python transcribe.py recording.raw > recording.json
+python process_recording.py
